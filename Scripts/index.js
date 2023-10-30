c39f2a390e20ee2ff02773da3f832a6831b43517
IgnacioCG28 <ignaciocarmonagonzalez@alumnos.tuniversformacion.es>
2023-10-30 09:28:53 +0100

    cambio a arrow funct por comodidad  y añadir cosas nuevas

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,29 +1,35 @@
 let saldo = 5000;
 let PIN = "1234";
-
+// Llamadas
 const deposit = document.getElementById("deposit");
 const retire = document.getElementById("retire");
 const transfer = document.getElementById("transfer");
+const password = document.getElementById("changePass");
+const showSaldo = document.getElementById("cajeroSaldo");
 
-window.addEventListener("load",checkPass);
+// Evento al cargar
+window.addEventListener("load",() => {
+    const response= prompt("¿Cuál es el PIN?")
+    if (response !== PIN) {
+        alert("Error")
+    }else{
+        showSaldo.innerHTML = `Su saldo es ${saldo}`;
+    }
+});
 
+//Eventos
 
-deposit.addEventListener("click",depo);
-retire.addEventListener("click", reti);
-transfer.addEventListener("click", transf)
-
-
-function depo() {
+deposit.addEventListener("click",() => {
     const depo = parseFloat(prompt("Ingrese cantidad para depositar:"));
     if (isNaN(depo) || depo <= 0) {
-      alert("Cantidad inválida. Intente de nuevo.");
+      alert("Inválida. Intentelo de nuevo.");
     } else {
       saldo += depo;
-      alert(`Se han depositado ${depo.toFixed(2)} €`);
+      alert(`Se han depositado ${depo.toFixed(2)} dinares`);
     }
-  }
+});
 
-function reti() {
+retire.addEventListener("click", () =>{
     const reti = parseFloat(prompt("Ingrese cantidad a retirar:"));
     if (isNaN(reti) || reti <= 0 || reti > saldo) {
       alert("Cantidad inválida o insuficiente. Intente de nuevo.");
@@ -31,19 +37,24 @@ function reti() {
       saldo -= reti;
       alert(`Ha retirado ${reti.toFixed(2)} €`);
     }
-  }
+});
 
-function transf() {
+transfer.addEventListener("click", () => {
     const cuenta = prompt("Ingrese la cuenta a la que desee ingresar el dinero:")
-    const transf = prompt("Ingrese cantidad a tranferir:")
-}
-
-
-
-function checkPass() {
-   const response= prompt("¿Cuál es el PIN?")
-    if (response !== PIN) {
-        alert("Error")
+    const transf = parseFloat(prompt("Ingrese cantidad a tranferir:"));
+    if (isNaN(transf || transf <= 0 || transf >= saldo)) {
+        alert("No tienes esos dinares")
+    } else {
+        const response = alert(`Has ingresado ${transf}€ a la cuenta ${cuenta}`);
     }
-}
+});
+
+password.addEventListener("click", () =>{
+const pass = parseInt(prompt("Escriba la nueva contraseña de digitos:"))
+if (isNaN(pass)) {
+    alert("Te he dicho digitos.")
+} else {
+    PIN = pass;
+} 
+});
 
